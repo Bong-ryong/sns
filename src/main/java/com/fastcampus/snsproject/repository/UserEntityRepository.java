4fe87991dd1da6be810c2775e9b9d33d718392a6
DESKTOP-GMTVVBQ\Yeo BongRyong <diadooly@naver.com>
2023-02-11 17:55:44 +0900

    sns project

---
+++ src/main/java/com/fastcampus/snsproject/repository/UserEntityRepository.java
@@ -0,0 +1,2 @@
+package com.fastcampus.snsproject.repository;public interface UserEntityRepository {
+}
---
+++ src/test/java/com/fastcampus/snsproject/fixture/UserEntityFixture.java
@@ -0,0 +1,2 @@
+package com.fastcampus.snsproject.feature;public class UserEntityFixture {
+}
